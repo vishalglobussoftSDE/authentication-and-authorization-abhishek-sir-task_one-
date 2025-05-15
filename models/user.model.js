c09bd0171a083a0f87c4712232be38399f0923fc
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-15 15:17:26 +0530

    write user model

---
+++ models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImage: {
+      type: String, // Cloudinary URL or local path
+      default: "",
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false, // becomes true after OTP verification
+    },
+
+    otp: {
+      code: String,       // Store hashed OTP
+      expiresAt: Date,    // Optional: set expiry for OTP
+    },
+
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
